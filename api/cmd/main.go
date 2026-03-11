2487a4fce523a35389752f2e2480f4fb4cbfd781
faissalmaulana <114222238+faissalmaulana@users.noreply.github.com>
2026-03-11 17:08:04 +0700

    inject new dependencies

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/faissalmaulana/go-approve/cmd/handlers"
 	"github.com/faissalmaulana/go-approve/internal/db"
 	"github.com/faissalmaulana/go-approve/internal/repository/user"
+	"github.com/faissalmaulana/go-approve/internal/service/auth"
+	"github.com/faissalmaulana/go-approve/internal/service/jwtfx"
 	"go.uber.org/fx"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
@@ -13,9 +15,11 @@ import (
 
 func main() {
 	fx.New(
+		jwtfx.Module,
 		fx.Provide(
 			NewEchoMux,
 			NewHttpServer,
+			auth.New,
 			db.New,
 			fx.Annotate(user.New, fx.As(new(user.UserStorage))),
 			handlers.NewHealthHandler,
