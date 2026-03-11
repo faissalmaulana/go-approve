69d52a0d77d6e87f7c66dc2f625705ef348a09cc
faissalmaulana <114222238+faissalmaulana@users.noreply.github.com>
2026-03-11 18:11:34 +0700

    add new errors constant

--- api/internal/service/errors.go
+++ api/internal/service/errors.go
@@ -3,8 +3,10 @@ package service
 import "errors"
 
 var (
-	ErrDuplicatedUser = errors.New("user already exists")
-	ErrInternal       = errors.New("something went wrong")
-	ErrSubIsEmpty     = errors.New("jwt subject is required")
-	ErrInvalidPayload = errors.New("invalid payload")
+	ErrDuplicatedUser     = errors.New("user already exists")
+	ErrUserNotFound       = errors.New("user haven't registered yet")
+	ErrPasswordNotMatched = errors.New("password not matched")
+	ErrInternal           = errors.New("something went wrong")
+	ErrSubIsEmpty         = errors.New("jwt subject is required")
+	ErrInvalidPayload     = errors.New("invalid payload")
 )
