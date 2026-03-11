f539547b2070a5d877e5e55e744bb29ef265f9c8
faissalmaulana <114222238+faissalmaulana@users.noreply.github.com>
2026-03-11 17:07:26 +0700

    add jwt config

---
+++ api/internal/service/jwtfx/config.go
@@ -0,0 +1,20 @@
+package jwtfx
+
+import (
+	"os"
+	"time"
+)
+
+type Config struct {
+	SecretKey string
+	Issuer    string
+	Expire    time.Duration
+}
+
+func NewConfig() *Config {
+	return &Config{
+		SecretKey: os.Getenv("JWT_SECRET"),
+		Issuer:    os.Getenv("JWT_ISSUER"),
+		Expire:    24 * time.Hour,
+	}
+}
