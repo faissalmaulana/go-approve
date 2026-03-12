6639a5c34f4180873fea1f25b1dcdcd2d7365d54
faissalmaulana <114222238+faissalmaulana@users.noreply.github.com>
2026-03-13 01:46:35 +0700

    migrate: users table

---
+++ api/migrations/000001_create_users.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+    id       VARCHAR(36)  PRIMARY KEY,
+    name     VARCHAR(100) NOT NULL,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(60)  NOT NULL,
+    handler  VARCHAR(50)  NOT NULL UNIQUE
+);
